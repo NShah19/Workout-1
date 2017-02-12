571aa32f73b48c8816714d522b96b294378b1b20
[Terrence Ho] <[terrenceho.books@gmail.com]>
2017-02-11 20:57:20 -0800

    Got Sound

--- app/src/main/java/com/example/kho/workout/startWorkActivity.java
+++ app/src/main/java/com/example/kho/workout/startWorkActivity.java
@@ -1,117 +1,56 @@
 package com.example.kho.workout;
 
-import android.content.Intent;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
+import android.speech.tts.TextToSpeech;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.List;
 import java.util.Locale;
 
 import static android.speech.tts.TextToSpeech.Engine.KEY_PARAM_STREAM;
 
-public class startWorkActivity extends AppCompatActivity{
-    private TextToSpeech tts;
-    private Button speakButton;
-    private EditText inputText;
+public class startWorkActivity extends AppCompatActivity implements TextToSpeech.OnInitListener{
+    EditText getTextToSpeak;
+    Button speak;
+    TextToSpeech tts;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_work);
 
-//        inputText = (EditText) findViewById(R.id.editText1);
-//        tts = new TextToSpeech(this, this);
-//
-//        speakButton = (Button) findViewById(R.id.button1);
-//        // button on click event
-//        speakButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View arg0) {
-//                speakText();
-//            }
-//
-//        });
+        tts = new TextToSpeech(this, this);
+//        getTextToSpeak = (EditText) findViewById(R.id.editTextT);
+        speak = (Button)findViewById(R.id.startButton);
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakOutNow();
+            }
+        });
+
+
     }
 
-//    @Override
-//    public void onDestroy() {
-//        // Don't forget to shutdown tts!
-//        if (tts != null) {
-//            tts.stop();
-//            tts.shutdown();
-//        }
-//        super.onDestroy();
-//    }
-//
-//    @Override
-//    public void onInit(int status) {
-//
-//        if (status == TextToSpeech.SUCCESS) {
-//
-//            int result = tts.setLanguage(Locale.US);
-//
-//            if (result == TextToSpeech.LANG_MISSING_DATA
-//                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-//                Log.e("TTS", "This Language is not supported");
-//            } else {
-//                speakButton.setEnabled(true);
-//                speakText();
-//            }
-//
-//        } else {
-//            Log.e("TTS", "Initilization Failed!");
-//        }
-//
-//    }
-//
-//    private void speakText() {
-//
-//        CharSequence toSpeak = inputText.getText().toString();
-//
-//        tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
-//    }
+    @Override
+    public void onInit (int text) {
+        if(text == TextToSpeech.SUCCESS){
+            int language = tts.setLanguage(Locale.ENGLISH);
+            if(language == TextToSpeech.LANG_MISSING_DATA || language == TextToSpeech.LANG_NOT_SUPPORTED){
+                speak.setEnabled(true);
+                speakOutNow();
+            } else {
 
-//    private static final int SPEECH_REQUEST_CODE = 0;
-//    Button startWorkout;
-//    TextToSpeech engine;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_start_work);
-//
-//        engine = new TextToSpeech(this, this);
-//
-//        startWorkout = (Button)findViewById(R.id.startButton);
-//        startWorkout.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View view) {
-////                speech();
-//                engine.speak("Beginning", TextToSpeech.QUEUE_ADD, Bundle(KEY_PARAM_STREAM), "ID");
-//            }
-//        });
-//
-//    }
-//
-//    @Override
-//    public void onInit(int status) {
-//        Log.d("Speech", "OnInit - Status ["+status+"]");
-//        if (status == TextToSpeech.SUCCESS) {
-//            Log.d("Speech", "Success!");
-//            engine.setLanguage(Locale.UK);
-//        }
-//    }
-//
-//
-//    private void speech() {
-//        engine.speak("Beginning Workout.  Say Done to continue exercise.", TextToSpeech.QUEUE_FLUSH, null, null);
-//    }
+            }
+        } else {
 
+        }
+    }
+
+    private void speakOutNow (){
+        String text = "Beginning Workout";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
 }
